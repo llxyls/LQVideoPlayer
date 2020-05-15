eac59e3d937194fc4a957de57aba3b38ff803f6e
lq <1778963303@qq.com>
2020-05-15 14:39:56 +0800

    1111

---
+++ LQVideoPlayer/LQVideoPlayer/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  LQVideoPlayer
+//
+//  Created by 刘奇 on 2020/5/15.
+//  Copyright © 2020 liuqi. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+
+    var player: AVPlayer?
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        var item = AVPlayerItem(url: URL(string: "https://www.baidu.com") ?? <#default value#>)
+        
+        
+        
+    }
+
+
+}
+
